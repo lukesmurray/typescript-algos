56ffde6900209450bb8d7921c9e38e0e22053a86
Luke Murray <lukepigeonmail@gmail.com>
2020-11-25 13:12:12 -0500

    unpkg deploy

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      index: path.resolve(__dirname, "./build/esm/index.js"),
+    },
+    output: {
+      path: path.resolve(__dirname, "./build/umd"), // builds to ./build/umd/
+      filename: "[name].js", // index.js
+      library: "typescriptAlgos", // aka window.myLibrary
+      libraryTarget: "umd", // supports commonjs, amd and web browsers
+      globalObject: "this",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(tsx?)|(js)$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
